b2052bd3417742be2f8ea937946f7d8a08855c34
ryan.p <ryan.p@endurance.com>
2018-01-13 16:40:17 +0530

    day one

---
+++ wp-content/themes/logicboxes/footer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package LogicBoxes
+ */
+
+?>
+
+	</div><!-- #content -->
+
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+				Copyright All Rights Reserved © 2018
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
